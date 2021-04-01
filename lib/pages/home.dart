e5edc76ecbabf0ed7b7c36edac356c073342cdb0
Hamza-ye <7amza.it@gmail.com>
2021-04-01 16:41:22 +0300

    add padding

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -56,6 +56,7 @@ class _HomeState extends State<Home> {
 
   _buildListViewSeparated(AsyncSnapshot snapshot) {
     return ListView.separated(
+      padding: EdgeInsets.all(16.0),
       // itemBuilder: itemBuilder, separatorBuilder: separatorBuilder, itemCount: itemCount
       itemCount: snapshot.data.length,
       separatorBuilder: (context, index) => Divider(),
